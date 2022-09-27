f0ad1e91666c5c7a361d3ec63c761704f212887f
FaizciShylock <batuhantosunn@hotmail.com>
2022-09-27 18:53:11 +0300

    some changes happened

---
+++ some_unnecessary_functions/stores_functions.py
@@ -0,0 +1,11 @@
+import os
+import sys
+
+
+def say_my_name_func(which_part):
+    if which_part == "name":
+        print("Your name is: ","Batuhan")
+    elif which_part == "surname":
+        print("Your surname is: ","Tosun")
+    else:
+        print("What the hell !!!")
